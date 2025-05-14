d47b1568279e1b6b97af41fc13a290e44b616d5e
Waqas-Khan-CodeCanvas <waqaskhan11411704@gmail.com>
2025-05-14 23:09:55 +0500

    modefied code

--- 1-05-29025-fourth-paper/code_section/questions.py
+++ 1-05-29025-fourth-paper/code_section/questions.py
@@ -1,13 +1,25 @@
-PROCEDURE Select(Start, End : INTEGER)
-   DECLARE ThisNum, Total : INTEGER
-   DECLARE LastDigit, SecondLastDigit : INTEGER   
-   FOR ThisNum ← Start + 1 TO End - 1
-      LastDigit ← ThisNum MOD 10
-      SecondLastDigit ← (ThisNum DIV 10) MOD 10
-      Total ← LastDigit + SecondLastDigit
+# PROCEDURE Select(Start, End : INTEGER)
+#    DECLARE ThisNum, Total : INTEGER
+#    DECLARE LastDigit, SecondLastDigit : INTEGER   
+#    FOR ThisNum ← Start + 1 TO End - 1
+#       LastDigit ← ThisNum MOD 10
+#       SecondLastDigit ← (ThisNum DIV 10) MOD 10
+#       Total ← LastDigit + SecondLastDigit
 
-      IF Total = 6 THEN
-         OUTPUT ThisNum
-      ENDIF
-   NEXT ThisNum
-ENDPROCEDURE
+#       IF Total = 6 THEN
+#          OUTPUT ThisNum
+#       ENDIF
+#    NEXT ThisNum
+# ENDPROCEDURE
+
+def Select(Start, End):
+    for ThisNum in range(Start + 1, End):
+        LastDigit = ThisNum % 10
+        SecondLastDigit = (ThisNum // 10) % 10
+        Total = LastDigit + SecondLastDigit
+
+        if Total == 6:
+            print(ThisNum)
+
+# Example usage
+Select(10, 100)
